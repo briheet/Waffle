de3ff1547bfcbdead5ee09abda9e3d201a68f752
briheet01 <briheetyadav@gmail.com>
2024-02-04 17:09:15 +0530

    added upgrader

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 type Person struct {
 	Name  string `json:"name"`
 	Age   int64  `json:"age"`
@@ -7,5 +15,22 @@ type Person struct {
 	About string `json:"about"`
 }
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("Client Connected")
+	conn.Close()
+}
+
 func main() {
+	http.HandleFunc("/ws", wsHandler)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
